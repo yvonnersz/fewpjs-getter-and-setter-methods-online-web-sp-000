a0cd5605e761dd0de015b06f4d22c5348c17a4db
Yvonne M Pham <yvonnersz@gmail.com>
2020-08-11 13:17:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ class Circle {
   }
 
   set circumference(newCircumference) {
-    return this.radius = (Math.PI/2)
+    return this.radius = (newcircumference/(2*Math.PI))
   }
 
   set area(newArea) {
